93398f0d11ec35f2a8609770024bf9eec46cc86c
Anna Bedell <awbedell@gmail.com>
2015-06-12 12:26:02 -0400

    move_up move_down but with bugs

--- app/models/question.rb
+++ app/models/question.rb
@@ -4,4 +4,18 @@ class Question < ActiveRecord::Base
 
   validates :text, presence: true
 
+  def move_down
+    next_question = Question.where(number: (self.number + 1))
+    num = self.number
+    self.update(number: next_question.number)
+    next_question.update(number: num)
+  endz
+
+  def move_up
+    last_question = Question.where(number: (self.number - 1))
+    num = self.number
+    self.update(number: last_question.number)
+    last_question.update(number: num)
+  end
+
 end
