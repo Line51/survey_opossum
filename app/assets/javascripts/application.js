c1a650e218f15bcfd853f44cbbd43e449b35be0d
Brendan Collins <brendan.c.collins@gmail.com>
2015-06-15 14:29:57 -0400

    javascript can add questions

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,49 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+
+$(function () {
+      var counter = 0;
+  if (!$('.survey-form').length) {
+    return;
+  }
+    $('.add-question').on('click', function (e) {
+    e.preventDefault();
+    counter++;
+    addnewQuestion ('newQuestion');
+   
+ 
+    
+   
+  })
+  
+   $('.question-field').on('change', 'select', function () {
+    if($('select').val() === 'multi')  {
+      alert('yay');
+      showAnswerfield('answerChoice');
+    }
+  })
+ 
+  
+ 
+
+});
+
+ 
+
+
+function addnewQuestion(template, model) {
+
+  var fn = _.template($('#' + template).html(), { variable: 'm' });
+  var newhtml= fn(model);
+   newhtml = newhtml.replace( /\[[0-9]+\]/g,'[' + $('.question-wrapper').length + ']');
+  $('.question-field').append(newhtml);
+  console.log(fn);
+}
+
+function showAnswerfield(template, model) {
+
+  var fn = _.template($('#' + template).html(), { variable: 'm' });
+  $('.answer-choices-field').html(fn(model));
+  console.log(fn);
+}
